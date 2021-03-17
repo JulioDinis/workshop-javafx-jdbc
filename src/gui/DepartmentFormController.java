60f7a96456b02831c8bd141ac124465324d20a73
Julio Dinis <lima.juliolima.julio45@gmail.com>
2021-03-17 15:18:41 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -5,9 +5,14 @@
  */
 package gui;
 
+import gui.util.Constraints;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +21,42 @@ import javafx.fxml.Initializable;
  */
 public class DepartmentFormController implements Initializable {
 
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErroName;
+
+    @FXML
+    private Button btSalve;
+
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    public void onBtSaveAction() {
+        System.out.println("onBtSaveAction");
+    }
+
+    @FXML
+    public void onBtCancelAction() {
+        System.out.println("onBtCancelAction");
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
 }
