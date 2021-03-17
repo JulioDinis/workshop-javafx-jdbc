8ca0cea858bb4ee50e718b933b9f4f386741be4c
Julio Dinis <lima.juliolima.julio45@gmail.com>
2021-03-17 11:43:30 -0300

    About View

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -5,11 +5,19 @@
  */
 package controller;
 
+import application.Main;
+import gui.util.Alerts;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -39,7 +47,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemAboutAction() {
-        System.out.println("onMenuItemAbout");
+        loadView("/gui/About.fxml");
     }
 
     /**
@@ -48,6 +56,29 @@ public class MainViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    // Carrega a view - o synchronized garante que o carregamento não será interrompido no meio
+    private synchronized void loadView(String absoluteName) {
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+            // Carrega o Scene Principal
+            Scene mainScene = Main.getMainScene();
+            // Pega o VBox da classe principal onde será inserido os node
+            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            // Guarda o node da barra de menu
+            Node mainMenu = mainVbox.getChildren().get(0);
+            mainVbox.getChildren().clear(); //limpa o VBox da Main
+            mainVbox.getChildren().add(mainMenu); // Adiciona o node da Barra de Menu
+            mainVbox.getChildren().addAll(newVBox.getChildren()); // Adiciona os node da tela about
+
+        } catch (Exception e) {
+            Alerts.showAlert("IO Exeption", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+
     }
 
 }
