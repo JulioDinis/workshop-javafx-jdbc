c17c45284bd85a0b14cc3d343541bed9ff92de4c
Julio Dinis <lima.juliolima.julio45@gmail.com>
2021-03-17 09:15:05 -0300

    Local project create

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author julio
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
