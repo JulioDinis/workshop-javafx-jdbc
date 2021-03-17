1f049f63fba8627bdeb932a24d01fbc004af3c40
Julio Dinis <lima.juliolima.julio45@gmail.com>
2021-03-17 12:07:24 -0300

    DepartmentList controller

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -5,9 +5,17 @@
  */
 package gui;
 
+import application.Main;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
 
 /**
  * FXML Controller class
@@ -16,12 +24,38 @@ import javafx.fxml.Initializable;
  */
 public class DepartmentListController implements Initializable {
 
+    @FXML
+    private TableView<Department> tableViewDepartment;
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private Button btNew;
+
+    @FXML
+    public void onBtNewAction() {
+        System.out.println("onBtNewAction");
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        
+        tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+        
+    }
+
 }
